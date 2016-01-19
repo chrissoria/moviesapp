cc68d4c6992e66c8cad11ca857d766a27ec483c7
chrissoria <csoria1@mail.ccsf.edu>
2016-01-18 21:08:45 -0800

    dataPopulatedContinued
    data is now arranged with a title and overview for each movie in each
    cell.

---
+++ MovieCell.swift
@@ -0,0 +1,32 @@
+//
+//  MovieCell.swift
+//  moviesapp
+//
+//  Created by christopher soria on 1/18/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
